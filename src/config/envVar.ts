27eef41ae981fddafe78d85d3e1557738406d04d
Nam Phan <namph.tech@gmail.com>
2023-04-27 15:21:41 +0000

    ✨ Remove package 'config'

--- src/config/envVar.ts
+++ src/config/envVar.ts
@@ -1,22 +1,20 @@
-import config from 'config';
 
 /**
  * Database variables
  * 
  */
-// todo: remove all config.get
-export const dbHost = process.env.DB_HOST || config.get('db.host');
-export const dbPort = process.env.DB_PORT || config.get('db.port');
-export const dbName = process.env.DB_NAME || config.get('db.database');
-export const dbUser = process.env.DB_USER || config.get('db.username');
-export const dbPassword = process.env.DB_PASSWORD || config.get('db.password');
-export const dbType = process.env.DB_TYPE || config.get('db.type');
+export const dbHost = process.env.DB_HOST;
+export const dbPort = process.env.DB_PORT;
+export const dbName = process.env.DB_NAME;
+export const dbUser = process.env.DB_USER;
+export const dbPassword = process.env.DB_PASSWORD;
+export const dbType = process.env.DB_TYPE;
 export const nodeEnv = process.env.NODE_ENV || 'development';
 
 /**
  * App variables
  */
-export const appPort = process.env.APP_PORT || config.get('server.port');
+export const appPort = process.env.APP_PORT;
 
 
 export default {
